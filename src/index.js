525fa4b0373523aa39bb820896596c73d70c1344
hemantgabra <hemantgabra.designer@gmail.com>
2025-02-09 19:28:29 +0530

    Study fetch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
-// import App from './App';
+import App from './App';
 import Home from "./Home";
 import AboutUs from "./AboutUs";
 import Contact from "./Contact";
@@ -17,7 +17,7 @@ root.render(
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<App />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>

4c6a9fd050669953c84b6341e0b734f412b22527
hemantgabra <hemantgabra.designer@gmail.com>
2025-03-02 17:49:29 +0530

    Fixed json data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import StudentData from "./Components/StudentData";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+import Contact from "./Contact";
+//import StudentData from "./Components/StudentData";
 
-let newData = [];
-async function fetchData() {
-  try {
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    let json = await response.json();
-    console.log("Fetched Data:", json);
-    newData = json;
-    return json;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
 
-fetchData().then((newData) => {
-  console.log("latestData", newData);
-});
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is initial Poject10</h1>
-      <StudentData studentPropsdata={newData}/>
+    <BrowserRouter>
+    <div className="sm:bg-red-500">
+    <div className="flex justify-between sm:flex-col md:flex-row p-5">
+      <div className="bg-blue-500">Logo</div>
+      <div className="bg-green-500">
+    <nav className="flex shadow-lg p-3 justify-center items-center text-2xl gap-4">
+      <Link to="/">Home</Link> |
+      <Link to="/aboutus">About</Link> |
+      <Link to="/contact">Contact</Link>
+    </nav>
     </div>
+    </div>
+    </div>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+    // <div className="App">
+    //   <h1>This is initial Poject10</h1>
+    //   <StudentData studentPropsdata={newData}/>
+    // </div>
   );
 }
 

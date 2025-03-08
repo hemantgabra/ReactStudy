598002b6d7f1d499fb28ee17b0ed22ad0bcccee6
hemantgabra <hemantgabra.designer@gmail.com>
2025-03-09 01:50:11 +0530

    Update vite

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function Home() {
+  // const [stuData, setStuData] = useState([]);
+  // const [loading, setLoadingState] = useState(true);
+
+  // useEffect(() => {
+  //   fetch("https://dummy-json.mock.beeceptor.com/posts")
+  //     .then((response) => response.json())
+  //     .then((data) => setStuData(data))
+  //     .catch((error) => console.error("Error:", error));
+  //   setLoadingState(false);
+  // }, []);
+
+  return (
+    <div>
+      <div>Footer will come here</div>
+    </div>
+  );
+}
+export default Home;

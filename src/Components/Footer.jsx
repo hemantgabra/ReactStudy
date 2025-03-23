0ca0fb96ecd13c898bc851fefb6917dbe9b79283
mohit <mk7514004@gmail.com>
2025-03-23 15:21:00 +0530

    Updated Work

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,3 +3,48 @@ function Footer() {
 }
 
 export default Footer;
+
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import { IoSearch } from "react-icons/io5";
+// import { FaChevronDown } from "react-icons/fa";
+
+
+
+// function Header() {
+
+//   // const [isOpen, setIsOpen] = useState(false);
+
+//   return <div>
+//     <BrowserRouter>
+//       <div className="shadow-md">
+//         <div className="flex justify-between items-center sm:flex-col md:flex-row px-3 py-2">
+//           <div className="py-1"><img src="images/Logoimage/Logo.svg" width="200" /></div>
+//           <div className=" border border-black-500 flex items-center  gap-[1rem] px-[1rem] w-[30rem] h-[2.5rem] rounded-3xl ">
+//             <IoSearch className="text-[1.6rem]" />
+//             <input type="text" placeholder="Search for Products, Brands and More"
+//               className="w-[100%] h-[100%] rounded-3xl flex items-center justify-center border-none outline-none "></input>
+//           </div>
+
+//           <div className="">
+
+//             <nav className="flex shadow-lg p-3 justify-center items-center text-[1.4rem] gap-10 relative">
+//               <button className="flex items-center gap-[0.8rem] rounded-lg px-2 py-[0.2rem] text-white bg-blue-700">Login
+//                 <FaChevronDown className="text-[1rem]" />
+//               </button>
+
+
+
+
+//               <Link to="/">Home</Link> |<Link to="/aboutus">About</Link> |
+//               <Link to="/contact">Contact</Link>
+//             </nav>
+//           </div>
+//         </div>
+//       </div>
+//     </BrowserRouter>
+
+
+//   </div>;
+// }
+
+// export default Header;

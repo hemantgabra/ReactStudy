28b4f63f8e0ec64d0e0a4d02902862852d797c0f
mohitkushwah14032000 <mk7514004@gmail.com>
2025-04-19 22:45:28 +0530

    slider grid fix

--- src/Components/CommonComponents/Carousel.jsx
+++ src/Components/CommonComponents/Carousel.jsx
@@ -1,31 +1,29 @@
 import { React, useEffect } from "react";
 import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+
 function AutoPlay(carouselImg) {
   const settings  = {
     dots: true,
     infinite: true,
-    speed: 2000,
     slidesToShow: 1,
-    // autoplay: true,
     slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
     autoplaySpeed: 2000,
     cssEase: "linear"
     
   };
 
-  // useEffect(()=>{(JSON.stringify(carouselImg))},[]);
-  // let myImgData = JSON.stringify(carouselImg);
-  // console.log("firstdata", myImgData);
-  // console.log("secondata", carouselImg)
+  
   return (
     <div className="slider-container w-full5263 py-6  mb-4 bg-[#f1f2f4]">
       <div className=" bg-[white] pb-7">
         <Slider {...settings}>
           {carouselImg.carouselImg.map((item) => (
             <div key={item.id}>
-              <img src={item.image} />
+              <img src={item.image}
+              className="w-full h-full object-contain"
+              />
             </div>
           ))}
         </Slider>
--- src/Components/CommonComponents/Gridbox.jsx
+++ src/Components/CommonComponents/Gridbox.jsx
@@ -8,12 +8,12 @@ function Festivesection(grid_deal) {
             {/* {JSON.stringify(grid_deal)} */}
 
 
-            
 
 
-            <div className="summer w-[30rem]">
-                <div className="text-2xl font-semibold px-[2rem] py-[1.3rem]  bg-[white] flex items-center justify-between">
-                    <p>Summer Fashion</p>
+
+            <div className="w-[33rem]">
+                <div className="text-2xl font-semibold px-[2rem] py-[2rem]  bg-[white] flex items-center justify-between">
+                    <p>Festive Must-Haves</p>
                     <FaArrowCircleRight className="text-blue-500" />
                 </div>
 
@@ -21,12 +21,9 @@ function Festivesection(grid_deal) {
                     {
                         grid_deal.grid_deal.map((item) => (
                             <div key={item.id} className="flex flex-col  border-2 border-gray-200 p-5 rounded-md">
-                                <img src={item.image} alt={item.name} className="w-full h-32 object-cover mb-2" />
-                                <h3 className="text-lg font-semibold">{item.name}</h3>
-                                <p className="text-gray-500">{item.description}</p>
-                                <p className="text-blue-500 font-semibold mt-2">{item.price}</p>
-                                
-                                
+                                <img src={item.image} alt={item.name} className="w-full h-32 object-cover mb-2 transition-transform duraction-300 hover:scale-105 " />
+                                <h3 className="text-lg font-semibold">{item.title}</h3>
+                                <p className="text-gray-500">{item.deal}</p>
                             </div>
                         ))}
                 </div>
